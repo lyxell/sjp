7dcbb74b9b84326ba4d288755a28e094e01d1a2d
lyxell <alyxell@kth.se>
2021-04-19 16:17:50 +0200

    Remove unused types

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ typedef std::chrono::high_resolution_clock hclock;
 
 int main(int argc, char** argv) {
     sjp::parser parser;
+    const char* filename = "Example.java";
 
     auto t1 = hclock::now();
-    parser.add_file("Example.java");
+    parser.add_file(filename);
     auto t2 = hclock::now();
     std::cout << "Time difference:"
         << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count() << " milliseconds" << std::endl;
@@ -16,7 +17,7 @@ int main(int argc, char** argv) {
     std::cout << "Time difference:"
         << std::chrono::duration_cast<std::chrono::milliseconds>(t3-t1).count() << " milliseconds" << std::endl;
     std::cout << "Parsed " << parser.num_asts() << " ASTs" << std::endl;
-    for (auto& [t, a, b] : parser.get_tuples()) {
+    for (auto& [t, a, b] : parser.get_tuples(filename)) {
         std::cout << t << " " << a << " " << b << std::endl;
     }
     return 0;
